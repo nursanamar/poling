cfea030bb42269818e898670d94d573e2f522754
Nursan amar <nursan011@gmail.com>
2018-01-27 22:31:46 +0800

    autocomplate kabupaten dari google

--- application/views/admin/addvote.php
+++ application/views/admin/addvote.php
@@ -29,7 +29,7 @@
                 <div class="form-group">
                   <label class="control-label col-md-3 col-sm-3">Kabupaten</label>
                   <div class="col-md-6 col-sm-6">
-                    <input required class="form-control col-md-7" type="text" name="kabupaten">
+                    <input id="area" required class="form-control col-md-7" type="text" name="kabupaten">
                   </div>
                 </div>
                 <div class="form-group">
@@ -54,3 +54,30 @@
     </div>
   </div>
 </div>
+<script type="text/javascript">
+function cariKabupaten() {
+      // Create the autocomplete object, restricting the search to geographical
+      // location types.
+      autocomplete = new google.maps.places.Autocomplete(
+          /** @type {!HTMLInputElement} */(document.getElementById('area')),
+          {
+            types: ['(cities)'],
+            region: 'ID',
+            componentRestrictions: {country: 'id'}
+          }
+        );
+        autocomplete.addListener('place_changed', function(){
+          var res = autocomplete.getPlace();
+          var kabupaten;
+          res.address_components.forEach(function(arr) {
+            if(arr.types[0] === 'administrative_area_level_2'){
+              kabupaten = arr.long_name;
+              $('#area').val(kabupaten);
+            }
+          })
+          console.log(res.address_components);
+        });
+    }
+</script>
+
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB9gAn0rzglRCeE-q18BZyzMz_oNymbRvA&libraries=places&language=id&callback=cariKabupaten" async defer></script>
